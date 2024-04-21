9a73afbd83d60a91220b46f9a4899b4c1eaa30bf
JKY45 <juankyespinosa@gmail.com>
2024-04-21 18:29:24 -0400

    formations

--- src/football_tools/line_up.py
+++ src/football_tools/line_up.py
@@ -9,13 +9,29 @@ class LineUpGrid(ABC):
         self.col: int = col
         self.player: int = -1
 
-    def conf_player(self, player: PlayerData):
-        self._set_statistics(player)
+    def conf_player(self, position: str, player: PlayerData):
+        in_position = False
+        for p in player.o_player_positions:
+            if p in position: 
+                in_position = True
+                break
+        self._set_statistics(player, in_position)
         self.player: int = player.dorsal
 
-    @abstractmethod
-    def _set_statistics(self, player_data: PlayerData) -> None:
-        pass
+    
+    def _set_statistics(self, player_data: PlayerData, in_position: bool) -> None:
+        if not in_position:
+            player_data.defending -= 5
+            player_data.dribbling -= 5
+            player_data.mentality_interceptions -= 5
+            player_data.mentality_vision -= 5
+            player_data.movement_reactions -= 5
+            player_data.skill_ball_control -= 5
+            player_data.passing -= 5
+            player_data.pace -= 5
+            player_data.skill_ball_control -= 5
+            player_data.shooting -= 5
+
 
 
 class LineUp(ABC):
@@ -24,7 +40,7 @@ class LineUp(ABC):
 
     def conf_players(self, players: Dict[str, PlayerData]) -> None:
         for k, v in players.items():
-            self.line_up[k].conf_player(v)
+            self.line_up[k].conf_player(k, v)
 
     def get_player_position(self, player: int) -> LineUpGrid | None:
         for k, v in self.line_up.items():
@@ -37,6 +53,139 @@ class LineUp(ABC):
         self.line_up = line_up.line_up
         self.conf_players(players)
 
+class Home433(LineUp):
+    def __init__(self) -> None:
+        super().__init__()
+        self.line_up = {
+            'GK': LineUpGrid(18, 5),
+            'RB': LineUpGrid(15, 8),
+            'CB1': LineUpGrid(15, 4),
+            'CB2': LineUpGrid(15, 6),
+            'LB': LineUpGrid(15, 2),
+            'CDM': LineUpGrid(12, 5),
+            'CM1': LineUpGrid(10, 7),
+            'CM2': LineUpGrid(10, 3),
+            'RW': LineUpGrid(6, 8),
+            'ST': LineUpGrid(6, 5),
+            'LW': LineUpGrid(6, 2)
+        }
+
+class Away433(LineUp):
+    def __init__(self) -> None:
+        super().__init__()
+        self.line_up = {
+            'GK': LineUpGrid(1, 5),
+            'RB': LineUpGrid(4, 2),
+            'CB1': LineUpGrid(4, 4),
+            'CB2': LineUpGrid(4, 6),
+            'LB': LineUpGrid(4, 8),
+            'CDM': LineUpGrid(7, 5),
+            'CM1': LineUpGrid(9, 7),
+            'CM2': LineUpGrid(9, 3),
+            'RW': LineUpGrid(13, 8),
+            'ST': LineUpGrid(13, 5),
+            'LW': LineUpGrid(13, 2)
+        }        
+class Home442(LineUp):
+    def __init__(self) -> None:
+        super().__init__()
+        self.line_up = {
+            'GK': LineUpGrid(18, 5),
+            'RB': LineUpGrid(15, 8),
+            'CB1': LineUpGrid(15, 4),
+            'CB2': LineUpGrid(15, 6),
+            'LB': LineUpGrid(15, 2),
+            'RM': LineUpGrid(10, 8),
+            'CM1': LineUpGrid(11, 6),
+            'CM2': LineUpGrid(11, 4),
+            'LM': LineUpGrid(10, 2),
+            'ST1': LineUpGrid(6, 4),
+            'ST2': LineUpGrid(6, 6)
+        }
+class Away442(LineUp):
+    def __init__(self) -> None:
+        super().__init__()
+        self.line_up = {
+            'GK': LineUpGrid(1, 5),
+            'RB': LineUpGrid(4, 2),
+            'CB1': LineUpGrid(4, 4),
+            'CB2': LineUpGrid(4, 6),
+            'LB': LineUpGrid(4, 8),
+            'RM': LineUpGrid(9, 8),
+            'CM1': LineUpGrid(8, 4),
+            'CM2': LineUpGrid(8, 6),
+            'LM': LineUpGrid(9, 2),
+            'ST1': LineUpGrid(13, 6),
+            'ST2': LineUpGrid(13, 4)
+        }    
+
+class Home343(LineUp):
+    def __init__(self) -> None:
+        super().__init__()
+        self.line_up = {
+            'GK': LineUpGrid(18, 5),
+            'CB1': LineUpGrid(15, 3),
+            'CB2': LineUpGrid(15, 5),
+            'CB3': LineUpGrid(15, 7),
+            'RM': LineUpGrid(10, 8),
+            'CM1': LineUpGrid(11, 6),
+            'CM2': LineUpGrid(11, 4),
+            'LM': LineUpGrid(10, 2),
+            'RW': LineUpGrid(6, 8),
+            'ST': LineUpGrid(6, 5),
+            'LW': LineUpGrid(6, 2)
+        }  
+
+class Away343(LineUp):
+    def __init__(self) -> None:
+        super().__init__()
+        self.line_up = {
+            'GK': LineUpGrid(1, 5),
+            'CB1': LineUpGrid(4, 3),
+            'CB2': LineUpGrid(4, 5),
+            'CB3': LineUpGrid(4, 7),
+            'RM': LineUpGrid(9, 8),
+            'CM1': LineUpGrid(8, 6),
+            'CM2': LineUpGrid(8, 4),
+            'LM': LineUpGrid(9, 2),
+            'RW': LineUpGrid(13, 8),
+            'ST': LineUpGrid(13, 5),
+            'LW': LineUpGrid(13, 2)
+        }
+
+class Home532(LineUp):
+    def __init__(self) -> None:
+        super().__init__()
+        self.line_up = {
+            'GK': LineUpGrid(18, 5),
+            'RB': LineUpGrid(14, 9),
+            'CB1': LineUpGrid(15, 3),
+            'CB2': LineUpGrid(15, 5),
+            'CB3': LineUpGrid(15, 7),
+            'LB': LineUpGrid(14, 1),
+            'CDM': LineUpGrid(12, 5),
+            'CM1': LineUpGrid(10, 7),
+            'CM2': LineUpGrid(10, 3),
+            'ST1': LineUpGrid(6, 4),
+            'ST2': LineUpGrid(6, 6)
+        }        
+
+class Away532(LineUp):
+    def __init__(self) -> None:
+        super().__init__()
+        self.line_up = {
+            'GK': LineUpGrid(1, 5),
+            'RB': LineUpGrid(5, 1),
+            'CB1': LineUpGrid(4, 3),
+            'CB2': LineUpGrid(4, 5),
+            'CB3': LineUpGrid(4, 7),
+            'LB': LineUpGrid(5, 9),
+            'CDM': LineUpGrid(7, 5),
+            'CM1': LineUpGrid(9, 7),
+            'CM2': LineUpGrid(9, 3),
+            'ST1': LineUpGrid(13, 6),
+            'ST2': LineUpGrid(13, 4)
+        }        
 
 class ProveLineUp(LineUp):
     def __init__(self) -> None:
