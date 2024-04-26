d88fca81334bd8c61928503b4d8a84c9711f9841
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2024-04-25 23:30:08 -0400

    llm module

--- src/main.py
+++ src/main.py
@@ -1,25 +1,29 @@
-import os
-from football_simulator.build_data import conf_game
-import time
+# import os
+# from football_simulator.build_data import conf_game
+# import time
 
 
-home_n = 'FC Barcelona'
-away_n = 'Real Madrid CF'
+# home_n = 'FC Barcelona'
+# away_n = 'Real Madrid CF'
 
-sim = conf_game(home_n, away_n)
-
-
-def clear_console():
-    if os.name == "posix":
-        os.system("clear")
-    elif os.name in ["ce", "nt", "dos"]:
-        os.system("cls")
+# sim = conf_game(home_n, away_n)
 
 
-a = time.time()
-for s in sim.simulate():
-    time.sleep(0.5)
-    clear_console()
-    print(s)
+# def clear_console():
+#     if os.name == "posix":
+#         os.system("clear")
+#     elif os.name in ["ce", "nt", "dos"]:
+#         os.system("cls")
 
-print(abs(a-time.time()))
+
+# a = time.time()
+# for s in sim.simulate():
+#     time.sleep(0.5)
+#     clear_console()
+#     print(s)
+
+# print(abs(a-time.time()))
+
+from football_llm.llm import query
+
+print(query('hola'))
