f253a2a3a1854a4c971441128fd24be2f30c7ae2
Alex <alexsierraalcala@gmail.com>
2024-04-28 23:05:12 -0400

    fixing fuzzy ofensive rules

--- src/football_agent/fuzzy_rules.py
+++ src/football_agent/fuzzy_rules.py
@@ -25,14 +25,47 @@ def fuzzy_defensive_position():
     defensive_position['normal'] = fuzz.trimf(defensive_position.universe, [0, 50, 100])
     defensive_position['good'] = fuzz.trimf(defensive_position.universe, [50, 100, 100])
 
-    rule1 = ctrl.Rule(distance_to_ball['low'], defensive_position['good'])
-    rule2 = ctrl.Rule(distance_to_ball['medium'], defensive_position['normal'])
-    rule3 = ctrl.Rule(distance_to_position['high'] & player_function['defense'], defensive_position['bad'])
-    rule4 = ctrl.Rule(distance_to_ball['high'] & player_function['midfield'], defensive_position['bad'])
-    rule5 = ctrl.Rule(distance_to_ball['high'] & player_function['attack'], defensive_position['normal'])
-    rule6 = ctrl.Rule(distance_to_ball['high'] & ~distance_to_position['high'] & player_function['defense'], defensive_position['normal'])
+     
 
-    defensive_position_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6])
+    rule1 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['low'] & player_function['defense'], defensive_position['good'])
+    rule2 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['low'] & player_function['midfield'], defensive_position['good'])
+    rule3 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['low'] & player_function['attack'], defensive_position['good'])
+
+    rule4 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['medium'] & player_function['defense'], defensive_position['good'])
+    rule5 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['medium'] & player_function['midfield'], defensive_position['normal'])
+    rule6 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['medium'] & player_function['attack'], defensive_position['normal'])
+
+    rule7 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['high'] & player_function['defense'], defensive_position['normal'])
+    rule8 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['high'] & player_function['midfield'], defensive_position['normal'])
+    rule9 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['high'] & player_function['attack'], defensive_position['bad'])
+
+    rule10 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['low'] & player_function['defense'], defensive_position['normal'])
+    rule11 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['low'] & player_function['midfield'], defensive_position['normal'])
+    rule12 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['low'] & player_function['attack'], defensive_position['normal'])
+
+    rule13 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['medium'] & player_function['defense'], defensive_position['normal'])
+    rule14 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['medium'] & player_function['midfield'], defensive_position['normal'])
+    rule15 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['medium'] & player_function['attack'], defensive_position['normal'])
+
+    rule16 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['high'] & player_function['defense'], defensive_position['bad'])
+    rule17 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['high'] & player_function['midfield'], defensive_position['bad'])
+    rule18 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['high'] & player_function['attack'], defensive_position['normal'])
+
+    rule19 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['low'] & player_function['defense'], defensive_position['bad'])
+    rule20 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['low'] & player_function['midfield'], defensive_position['normal'])
+    rule21 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['low'] & player_function['attack'], defensive_position['normal'])
+
+    rule22 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['medium'] & player_function['defense'], defensive_position['bad'])
+    rule23 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['medium'] & player_function['midfield'], defensive_position['bad'])
+    rule24 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['medium'] & player_function['attack'], defensive_position['normal'])
+
+    rule25 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['high'] & player_function['defense'], defensive_position['bad'])
+    rule26 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['high'] & player_function['midfield'], defensive_position['bad'])
+    rule27 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['high'] & player_function['attack'], defensive_position['bad'])
+
+    defensive_position_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, 
+                                                 rule10, rule11, rule12, rule13, rule14, rule15, rule16, rule17, rule18, 
+                                                 rule19, rule20, rule21, rule22, rule23, rule24, rule25, rule26, rule27])
     defensive_positioning = ctrl.ControlSystemSimulation(defensive_position_ctrl)
 
     return defensive_positioning
@@ -64,14 +97,45 @@ def fuzzy_ofensive_position():
     ofensive_position['normal'] = fuzz.trimf(ofensive_position.universe, [0, 50, 100])
     ofensive_position['good'] = fuzz.trimf(ofensive_position.universe, [50, 100, 100])
 
-    rule1 = ctrl.Rule(distance_to_ball['high'] & distance_to_enemy_goal['high'], ofensive_position['bad'])
-    rule2 = ctrl.Rule(~distance_to_ball['high'] & player_function['midfield'], ofensive_position['good'])
-    rule3 = ctrl.Rule(distance_to_position['low'] & ~distance_to_ball['high'], ofensive_position['good'])
-    rule4 = ctrl.Rule(distance_to_enemy_goal['high'] & player_function['attack'], ofensive_position['bad'])
-    rule5 = ctrl.Rule(~distance_to_ball['high'] & ~player_function['midfield'], ofensive_position['good'])
-    rule6 = ctrl.Rule(~distance_to_ball['high'] & ~distance_to_enemy_goal['high'] & ~distance_to_position['low'] & ~player_function['attack'], ofensive_position['normal'])
+    rule1 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['low'] & player_function['defense'], ofensive_position['good'])
+    rule2 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['low'] & player_function['midfield'], ofensive_position['good'])
+    rule3 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['low'] & player_function['attack'], ofensive_position['good'])
 
-    ofensive_position_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4])
+    rule4 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['medium'] & player_function['defense'], ofensive_position['good'])
+    rule5 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['medium'] & player_function['midfield'], ofensive_position['normal'])
+    rule6 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['medium'] & player_function['attack'], ofensive_position['normal'])
+
+    rule7 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['high'] & player_function['defense'], ofensive_position['normal'])
+    rule8 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['high'] & player_function['midfield'], ofensive_position['bad'])
+    rule9 = ctrl.Rule(distance_to_position['low'] & distance_to_ball['high'] & player_function['attack'], ofensive_position['normal'])
+
+    rule10 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['low'] & player_function['defense'], ofensive_position['normal'])
+    rule11 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['low'] & player_function['midfield'], ofensive_position['normal'])
+    rule12 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['low'] & player_function['attack'], ofensive_position['normal'])
+
+    rule13 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['medium'] & player_function['defense'], ofensive_position['normal'])
+    rule14 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['medium'] & player_function['midfield'], ofensive_position['normal'])
+    rule15 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['medium'] & player_function['attack'], ofensive_position['normal'])
+
+    rule16 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['high'] & player_function['defense'], ofensive_position['bad'])
+    rule17 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['high'] & player_function['midfield'], ofensive_position['bad'])
+    rule18 = ctrl.Rule(distance_to_position['medium'] & distance_to_ball['high'] & player_function['attack'], ofensive_position['normal'])
+
+    rule19 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['low'] & player_function['defense'], ofensive_position['bad'])
+    rule20 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['low'] & player_function['midfield'], ofensive_position['normal'])
+    rule21 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['low'] & player_function['attack'], ofensive_position['normal'])
+
+    rule22 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['medium'] & player_function['defense'], ofensive_position['bad'])
+    rule23 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['medium'] & player_function['midfield'], ofensive_position['bad'])
+    rule24 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['medium'] & player_function['attack'], ofensive_position['normal'])
+
+    rule25 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['high'] & player_function['defense'], ofensive_position['bad'])
+    rule26 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['high'] & player_function['midfield'], ofensive_position['bad'])
+    rule27 = ctrl.Rule(distance_to_position['high'] & distance_to_ball['high'] & player_function['attack'], ofensive_position['bad'])
+
+    ofensive_position_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, 
+                                                 rule10, rule11, rule12, rule13, rule14, rule15, rule16, rule17, rule18, 
+                                                 rule19, rule20, rule21, rule22, rule23, rule24, rule25, rule26, rule27])
     ofensive_positioning = ctrl.ControlSystemSimulation(ofensive_position_ctrl)
 
     return ofensive_positioning
